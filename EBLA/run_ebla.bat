33d1826a4c8ac6e588ae85c1492892d9911920ff
yoda2 <>
2003-08-07 21:09:14 +0000

    Added .bat files for compiling and running EBLA.

---
+++ EBLA/run_ebla.bat
@@ -0,0 +1,9 @@
+REM EXECUTE EBLA
+
+echo off
+
+REM $Id$
+
+java -classpath .;./jars/rowset.jar;./jars/pg73jdbc3.jar;./jars/jmf.jar;./jars/swingUtils.jar;./jars/utils.jar com.greatmindsworking.EBLA.Interfaces.EBLAGui
+
+REM $Log$
