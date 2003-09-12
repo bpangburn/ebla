0b83cd72527e9938f2b96e43438825cabc550636
yoda2 <>
2003-09-12 22:30:19 +0000

    Renaming compile_ebla.bat to wincompile.bat and run_ebla.bat to winrun.

--- EBLA/run_ebla.bat
+++
@@ -1,9 +0,0 @@
-REM EXECUTE EBLA
-
-echo off
-
-REM $Id$
-
-java -classpath .;./jars/rowset.jar;./jars/pg73jdbc3.jar;./jars/jmf.jar;./jars/swingUtils.jar;./jars/utils.jar com.greatmindsworking.EBLA.Interfaces.EBLAGui
-
-REM $Log$
