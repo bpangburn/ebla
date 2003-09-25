7d796ae51c2139bf2a66aa7bddd90fd6e8c41d16
yoda2 <>
2003-09-25 23:06:38 +0000

    Updates compilation and execution scripts to reflect GUI dependency on SwingSet project/JAR.

--- EBLA/wincompile.bat
+++ EBLA/wincompile.bat
@@ -8,4 +8,4 @@ echo off
 del .\com\greatmindsworking\EBLA\*.class
 del .\com\greatmindsworking\EBLA\Interfaces\*.class
 del .\com\greatmindsworking\EDISON\segm\*.class
-javac -classpath .;./jars/rowset.jar;./jars/pg73jdbc3.jar;./jars/jmf.jar;./jars/swingUtils.jar;./jars/utils.jar com/greatmindsworking/EBLA/Interfaces/EBLAGui.java
+javac -classpath .;./jars/rowset.jar;./jars/pg73jdbc3.jar;./jars/jmf.jar;./jars/swingSet.jar;./jars/utils.jar com/greatmindsworking/EBLA/Interfaces/EBLAGui.java
--- EBLA/winrun.bat
+++ EBLA/winrun.bat
@@ -3,4 +3,4 @@ REM
 REM WINDOWS EXECUTION SCRIPT FOR EBLA
 REM
 echo off
-java -classpath .;./jars/rowset.jar;./jars/pg73jdbc3.jar;./jars/jmf.jar;./jars/swingUtils.jar;./jars/utils.jar com.greatmindsworking.EBLA.Interfaces.EBLAGui
+java -classpath .;./jars/rowset.jar;./jars/pg73jdbc3.jar;./jars/jmf.jar;./jars/swingSet.jar;./jars/utils.jar com.greatmindsworking.EBLA.Interfaces.EBLAGui
