1c2e6d08850dd0f2bc9edb89a2fafbfbe3915327
yoda2 <>
2004-01-08 23:21:59 +0000

    Upgraded from Postgres 7.3 JDBC driver to 7.4.1 JDBC driver.

--- EBLA/winrun.bat
+++ EBLA/winrun.bat
@@ -3,4 +3,4 @@ REM
 REM WINDOWS EXECUTION SCRIPT FOR EBLA
 REM
 echo off
-java -classpath .;./jars/rowset.jar;./jars/pg73jdbc3.jar;./jars/jmf.jar;./jars/swingset-bin_0.6.0_beta.jar;./jars/utils.jar com.greatmindsworking.EBLA.Interfaces.EBLAGui
+java -classpath .;./jars/rowset.jar;./jars/pg74.1jdbc3.jar;./jars/jmf.jar;./jars/swingset-bin_0.6.0_beta.jar;./jars/utils.jar com.greatmindsworking.EBLA.Interfaces.EBLAGui
