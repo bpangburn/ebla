56fd95edd266b3170dddda291ace24c8d1a6ade0
yoda2 <>
2004-01-21 19:37:53 +0000

    Added compilation and execution Windows scripts for experimental jEDISON genetic training algorithm.

---
+++ EBLA/genrun.bat
@@ -0,0 +1,6 @@
+REM $Id$
+REM
+REM WINDOWS EXECUTION SCRIPT FOR jEDISON GENETIC TRAINING ALGORITHM
+REM
+echo off
+java -classpath .;./jars/pg74.1jdbc3.jar;./jars/utils.jar  com.greatmindsworking.EDISON.utils.GeneticSegmentation
