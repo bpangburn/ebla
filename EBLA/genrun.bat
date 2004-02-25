0c61320fb39bdf12ed39f43f75e46f4948ad8f59
yoda2 <>
2004-02-25 22:17:54 +0000

    Set to run with 256MB heap.

--- EBLA/genrun.bat
+++ EBLA/genrun.bat
@@ -3,4 +3,4 @@ REM
 REM WINDOWS EXECUTION SCRIPT FOR jEDISON GENETIC TRAINING ALGORITHM
 REM
 echo off
-java -classpath .;./jars/pg74.1jdbc3.jar;./jars/utils.jar  com.greatmindsworking.EDISON.utils.GeneticSegmentation
+java -Xmx256m -classpath .;./jars/pg74.1jdbc3.jar;./jars/utils.jar  com.greatmindsworking.EDISON.utils.GeneticSegmentation
