e76211ac5f5a75e6457568d64dc641abb7a5456a
yoda2 <>
2004-02-25 21:51:17 +0000

    Updated to latest SwingSet JAR (0.7.0-beta).

--- EBLA/wincompile.bat
+++ EBLA/wincompile.bat
@@ -8,4 +8,4 @@ echo off
 del .\com\greatmindsworking\EBLA\*.class
 del .\com\greatmindsworking\EBLA\Interfaces\*.class
 del .\com\greatmindsworking\EDISON\segm\*.class
-javac -classpath .;./jars/rowset.jar;./jars/pg74.1jdbc3.jar;./jars/jmf.jar;./jars/swingset-bin_0.6.0_beta.jar;./jars/utils.jar com/greatmindsworking/EBLA/Interfaces/EBLAGui.java
+javac -classpath .;./jars/rowset.jar;./jars/pg74.1jdbc3.jar;./jars/jmf.jar;./jars/swingset-bin_0.7.0_beta.jar;./jars/utils.jar com/greatmindsworking/EBLA/Interfaces/EBLAGui.java
