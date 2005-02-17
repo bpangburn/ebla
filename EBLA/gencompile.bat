b8e039ea0c7c24d43b6a969ce3070d45252cbebc
yoda2 <>
2005-02-17 02:50:24 +0000

    Updated windows compilation scripts to reflect new directory structure.

--- EBLA/gencompile.bat
+++ EBLA/gencompile.bat
@@ -5,5 +5,6 @@ REM
 REM DELETE ANY EXISTING .class FILES AND RE-COMPILE ALL .java FILES FOR jEDISON GENETIC TRAINING ALGORITHM
 REM
 echo off
-del .\com\greatmindsworking\EDISON\utils\*.class
-javac -classpath .;./jars/pg74.1jdbc3.jar;./jars/utils.jar  com/greatmindsworking/EDISON/utils/GeneticSegmentation.java
+rmdir .\bin /s
+mkdir .\bin
+javac -source 1.4 -target 1.4 -Xlint:deprecation -Xlint:unchecked -d .\bin -classpath .;.\jars\rowset.jar;.\jars\pg74.1jdbc3.jar;.\jars\utils.jar; .\src\com\greatmindsworking\EDISON\segm\*.java .\src\com\greatmindsworking\EDISON\utils\*.java
--- EBLA/wincompile.bat
+++ EBLA/wincompile.bat
@@ -5,7 +5,6 @@ REM
 REM DELETE ANY EXISTING .class FILES AND RE-COMPILE ALL .java FILES FOR EBLA
 REM
 echo off
-del .\com\greatmindsworking\EBLA\*.class
-del .\com\greatmindsworking\EBLA\Interfaces\*.class
-del .\com\greatmindsworking\EDISON\segm\*.class
-javac -source 1.4 -target 1.4 -Xlint:deprecation -Xlint:unchecked -classpath .;./jars/rowset.jar;./jars/pg74.1jdbc3.jar;./jars/jmf.jar;./jars/swingset-bin.jar;./jars/utils.jar com/greatmindsworking/EBLA/Interfaces/EBLAGui.java
+rmdir .\bin /s
+mkdir .\bin
+javac -source 1.4 -target 1.4 -Xlint:deprecation -Xlint:unchecked -d .\bin -classpath .;.\jars\rowset.jar;.\jars\pg74.1jdbc3.jar;.\jars\jmf.jar;.\jars\swingset-bin.jar;.\jars\utils.jar .\src\com\greatmindsworking\EDISON\segm\*.java .\src\com\greatmindsworking\EBLA\*.java .\src\com\greatmindsworking\EBLA\Interfaces\*.java
