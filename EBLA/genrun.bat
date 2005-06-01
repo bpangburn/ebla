94695a6f2390b49a4d575d6fef8bf40b950c0f95
yoda2 <>
2005-06-01 17:42:05 +0000

    Updated classpath such that it is not necessary to change to ./bin directory to execute EBLA.

--- EBLA/genrun.bat
+++ EBLA/genrun.bat
@@ -3,5 +3,4 @@ REM
 REM WINDOWS EXECUTION SCRIPT FOR jEDISON GENETIC TRAINING ALGORITHM
 REM
 echo off
-cd .\bin
-java -Xmx256m -classpath .;./jars/postgresql-8.0-311.jdbc3.jar;./jars/utils.jar  com.greatmindsworking.EDISON.utils.GeneticSegmentation
+java -Xmx256m -classpath .\bin;.\jars\postgresql-8.0-311.jdbc3.jar;.\jars\utils.jar com.greatmindsworking.EDISON.utils.GeneticSegmentation
--- EBLA/winrun.bat
+++ EBLA/winrun.bat
@@ -3,5 +3,4 @@ REM
 REM WINDOWS EXECUTION SCRIPT FOR EBLA
 REM
 echo off
-cd .\bin
-java -classpath .;..\jars\rowset.jar;..\jars\postgresql-8.0-311.jdbc3.jar;..\jars\jmf.jar;..\jars\swingset-bin.jar; com.greatmindsworking.EBLA.Interfaces.EBLAGui
+java -classpath .\bin;.\jars\rowset.jar;.\jars\postgresql-8.0-311.jdbc3.jar;.\jars\jmf.jar;.\jars\swingset-bin.jar com.greatmindsworking.EBLA.Interfaces.EBLAGui
