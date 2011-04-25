2ac823c24c878fcfde9adcd00ee6b0484a0e466f
yoda2 <>
2011-04-25 03:49:28 +0000

    Showing all recommended compiler warnings.

--- EBLA/wincompile.bat
+++ EBLA/wincompile.bat
@@ -8,4 +8,4 @@ REM echo off
 rmdir .\bin /s
 mkdir .\bin
 REM javac -source 1.6 -target 1.6 -Xlint:deprecation -Xlint:unchecked -d .\bin -classpath .;.\jars\rowset.jar;.\jars\postgresql-8.4-701.jdbc4.jar;.\jars\jmf.jar;.\jars\swingset-bin.jar .\src\com\greatmindsworking\utils\*.java .\src\com\greatmindsworking\EDISON\segm\*.java .\src\com\greatmindsworking\EBLA\*.java .\src\com\greatmindsworking\EBLA\Interfaces\*.java
-javac -source 1.6 -target 1.6 -Xlint:deprecation -Xlint:unchecked -d .\bin -classpath .;.\jars\rowset.jar;.\jars\h2-1.3.154.jar;.\jars\jmf.jar;.\jars\swingset-bin.jar .\src\com\greatmindsworking\utils\*.java .\src\com\greatmindsworking\EDISON\segm\*.java .\src\com\greatmindsworking\EBLA\*.java .\src\com\greatmindsworking\EBLA\Interfaces\*.java
+javac -source 1.6 -target 1.6 -Xlint -d .\bin -classpath .;.\jars\rowset.jar;.\jars\h2-1.3.154.jar;.\jars\jmf.jar;.\jars\swingset-bin.jar .\src\com\greatmindsworking\utils\*.java .\src\com\greatmindsworking\EDISON\segm\*.java .\src\com\greatmindsworking\EBLA\*.java .\src\com\greatmindsworking\EBLA\Interfaces\*.java
