f3b72444c5649b4fb42b3aeccfcef68c0169d98d
yoda2 <>
2011-04-28 20:20:08 +0000

    Replaced PostgreSQL jar with H2 jar.

--- EBLA/genrun.bat
+++ EBLA/genrun.bat
@@ -3,4 +3,4 @@ REM
 REM WINDOWS EXECUTION SCRIPT FOR jEDISON GENETIC TRAINING ALGORITHM
 REM
 echo off
-java -Xmx256m -classpath .\bin;.\jars\postgresql-8.4-701.jdbc4.jar com.greatmindsworking.EDISON.utils.GeneticSegmentation
+java -Xmx256m -classpath .\bin;.\jars\h2-1.3.154.jar com.greatmindsworking.EDISON.utils.GeneticSegmentation
