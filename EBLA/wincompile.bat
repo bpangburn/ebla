af09a331c8a529987de2409e17c07bde8beaf868
yoda2 <>
2011-04-28 19:34:17 +0000

    Removed PostgreSQL JDBC jar and added H2 jar. Updated source/target to Java 1.6. Showing all default warnings.

--- EBLA/wincompile.bat
+++ EBLA/wincompile.bat
@@ -4,8 +4,7 @@ REM WINDOWS COMPILATION SCRIPT FOR EBLA
 REM
 REM DELETE ANY EXISTING .class FILES AND RE-COMPILE ALL .java FILES FOR EBLA
 REM
-REM echo off
+echo off
 rmdir .\bin /s
 mkdir .\bin
-REM javac -source 1.6 -target 1.6 -Xlint:deprecation -Xlint:unchecked -d .\bin -classpath .;.\jars\rowset.jar;.\jars\postgresql-8.4-701.jdbc4.jar;.\jars\jmf.jar;.\jars\swingset-bin.jar .\src\com\greatmindsworking\utils\*.java .\src\com\greatmindsworking\EDISON\segm\*.java .\src\com\greatmindsworking\EBLA\*.java .\src\com\greatmindsworking\EBLA\Interfaces\*.java
-javac -source 1.6 -target 1.6 -Xlint -d .\bin -classpath .;.\jars\rowset.jar;.\jars\h2-1.3.154.jar;.\jars\jmf.jar;.\jars\swingset-bin.jar .\src\com\greatmindsworking\utils\*.java .\src\com\greatmindsworking\EDISON\segm\*.java .\src\com\greatmindsworking\EBLA\*.java .\src\com\greatmindsworking\EBLA\Interfaces\*.java
+javac -source 1.6 -target 1.6 -Xlint -d .\bin -classpath .;.\jars\h2-1.3.154.jar;.\jars\jmf.jar;.\jars\swingset-bin.jar .\src\com\greatmindsworking\utils\*.java .\src\com\greatmindsworking\EDISON\segm\*.java .\src\com\greatmindsworking\EBLA\*.java .\src\com\greatmindsworking\EBLA\Interfaces\*.java
